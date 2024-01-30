6d9d419da957f7fb9df0a5e63f261a8a52e7471f
sakir yucel <158056515+yucelsakir@users.noreply.github.com>
2024-01-30 23:13:21 +0300

    Update register_user.dart güncellendi

--- register_user.dart
+++ register_user.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'models/register_model.dart'; // Bu satırı ekledim
-import 'package:http/http.dart' as http; // Bu satırı ekledim
-import 'dart:convert'; // JSON verilerini işlemek için
-
-const String apiUrl = 'http://localhost:3000'; // Bu satırı ekledim
+import 'package:my_app/models/register_model.dart';
+import 'package:http/http.dart' as http; 
+import 'dart:convert'; 
 
 class RegisterUser extends StatefulWidget {
   const RegisterUser({super.key, required String title});
@@ -19,34 +17,26 @@ class RegisterUserState extends State<RegisterUser> {
   String adi = '';
   String soyadi = '';
   String eposta = '';
-  String kullanici_adi = '';
+  String kullaniciAdi = '';
   String sifre = '';
 
   Future save() async {
-    // MSSQL API'nin kullanıcı ekleme URL'sini oluştur
-    final Uri url =
-        Uri.parse('$apiUrl/kullanici_ekle'); // Bu satırı değiştirdim
+    final Uri url = Uri.parse('http://localhost:3000/kullanici_ekle');
 
-    // POST isteği için verileri hazırla
     final Map<String, String> data = {
       'adi': adi,
       'soyadi': soyadi,
       'eposta': eposta,
-      'kullanici_adi': kullanici_adi, // Bu satırı değiştirdim
+      'kullaniciAdi': kullaniciAdi,
       'sifre': sifre,
     };
 
-    // POST isteğini gönder
-    final response = await http.post(url, body: data); // Bu satırı değiştirdim
+    final response = await http.post(url, body: data);
 
-    // Yanıtı kontrol et
     if (response.statusCode == 200) {
-      // Yanıt başarılı ise, kullanıcı verilerini al
-      final user = RegisterModel.fromJson(
-          jsonDecode(response.body)); // Bu satırı değiştirdim
+      final user = RegisterModel.fromJson(jsonDecode(response.body));
       print('Kullanıcı kaydı başarılı: ${user.toJson()}');
     } else {
-      // Yanıt başarısız ise, hata mesajı göster
       print('Kullanıcı kaydı başarısız: ${response.body}');
     }
   }
@@ -62,7 +52,6 @@ class RegisterUserState extends State<RegisterUser> {
             overlayColor: MaterialStatePropertyAll(Colors.green),
           ),
         ),
-        //title: Text(widget.title),
         backgroundColor: const Color.fromARGB(255, 26, 34, 38),
         shadowColor: Colors.white,
         title: const Center(
@@ -77,116 +66,97 @@ class RegisterUserState extends State<RegisterUser> {
         child: Form(
           key: _formKey,
           child: Padding(
-            padding: const EdgeInsets.all(
-                16.0), // Form widget'ına kenarlardan boşluk ver
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
               children: <Widget>[
-                const Expanded(
-                  child: Image(
-                    image: AssetImage("lib/images/atik.jpg"),
-                    fit: BoxFit.scaleDown,
-                    width: 380,
-                    height: 380,
-                  ),
+                Image(
+                  image: AssetImage("lib/images/atik.jpg"),
+                  fit: BoxFit.scaleDown,
+                  width: 200,
+                  height: 200,
                 ),
                 const SizedBox(height: 16.0),
-                Expanded(
-                  child: TextFormField(
-                    // Adı soyadı widget'ının kontrolcüsünü ver
-                    decoration: const InputDecoration(
-                      labelText: 'Adı', // Adı soyadı widget'ının etiketini ver
-                      border:
-                          OutlineInputBorder(), // Adı soyadı widget'ının kenarlığını ver
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Lütfen adınızı giriniz';
-                      }
-                    },
-                    onSaved: (value) {
-                      adi = value!;
-                    },
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Adı',
+                    border: OutlineInputBorder(),
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Lütfen adınızı giriniz';
+                    }
+                  },
+                  onSaved: (value) {
+                    adi = value!;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText:
-                          'Soyadı', // Adı soyadı widget'ının etiketini ver
-                      border:
-                          OutlineInputBorder(), // Adı soyadı widget'ının kenarlığını ver
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Lütfen soyadınızı giriniz';
-                      }
-                    },
-                    onSaved: (value) {
-                      soyadi = value!;
-                    },
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Soyadı',
+                    border: OutlineInputBorder(),
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Lütfen soyadınızı giriniz';
+                    }
+                  },
+                  onSaved: (value) {
+                    soyadi = value!;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Eposta',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Lütfen e-posta adresinizi giriniz';
-                      }
-                    },
-                    onSaved: (value) {
-                      eposta = value!;
-                    },
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Eposta',
+                    border: OutlineInputBorder(),
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Lütfen e-posta adresinizi giriniz';
+                    }
+                  },
+                  onSaved: (value) {
+                    eposta = value!;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Kullanıcı Adı',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Lütfen kullanıcı adınızı giriniz';
-                      }
-                    },
-                    onSaved: (value) {
-                      kullanici_adi = value!;
-                    },
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Kullanıcı Adı',
+                    border: OutlineInputBorder(),
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Lütfen kullanıcı adınızı giriniz';
+                    }
+                  },
+                  onSaved: (value) {
+                    kullaniciAdi = value!;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Şifre',
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true, // Şifre widget'ının değerini gizle
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Lütfen şifrenizi girin';
-                      }
-                      if (value.length < 6) {
-                        return 'Lütfen en az 6 karakterlik bir şifre girin';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      sifre = value!;
-                    },
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Şifre',
+                    border: OutlineInputBorder(),
                   ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Lütfen şifrenizi girin';
+                    }
+                    if (value.length < 6) {
+                      return 'Lütfen en az 6 karakterlik bir şifre girin';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    sifre = value!;
+                  },
                 ),
                 const SizedBox(height: 16.0),
-                Expanded(
-                    child: ElevatedButton(
+                ElevatedButton(
                   child: const Text('Kaydet'),
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
@@ -194,7 +164,7 @@ class RegisterUserState extends State<RegisterUser> {
                       save();
                     }
                   },
-                )),
+                ),
               ],
             ),
           ),
