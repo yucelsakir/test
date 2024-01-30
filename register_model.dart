90c922e3ee2f509ce36ab6afd7147413157779e1
sakir yucel <158056515+yucelsakir@users.noreply.github.com>
2024-01-30 23:13:58 +0300

    Update register_model.dart güncellendi

--- register_model.dart
+++ register_model.dart
@@ -1,28 +1,28 @@
-class KullaniciEkle {
+class RegisterModel {
   String? adi;
   String? soyadi;
   String? eposta;
   String? kullaniciAdi;
   String? sifre;
 
-  KullaniciEkle(
+  RegisterModel(
       {this.adi, this.soyadi, this.eposta, this.kullaniciAdi, this.sifre});
 
-  KullaniciEkle.fromJson(Map<String, dynamic> json) {
+  RegisterModel.fromJson(Map<String, dynamic> json) {
     adi = json['adi'];
     soyadi = json['soyadi'];
     eposta = json['eposta'];
-    kullaniciAdi = json['kullanici_adi'];
+    kullaniciAdi = json['kullaniciAdi'];
     sifre = json['sifre'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['adi'] = this.adi;
-    data['soyadi'] = this.soyadi;
-    data['eposta'] = this.eposta;
-    data['kullanici_adi'] = this.kullaniciAdi;
-    data['sifre'] = this.sifre;
+    data['adi'] = adi;
+    data['soyadi'] = soyadi;
+    data['eposta'] = eposta;
+    data['kullaniciAdi'] = kullaniciAdi;
+    data['sifre'] = sifre;
     return data;
   }
 }
