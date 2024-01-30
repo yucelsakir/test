796892365dea4081d3dbdee7c5fd41293226f054
sakir yucel <158056515+yucelsakir@users.noreply.github.com>
2024-01-31 01:04:08 +0300

    Update register_model.dart son hali kayıt yapıyor

--- register_model.dart
+++ register_model.dart
@@ -8,21 +8,28 @@ class RegisterModel {
   RegisterModel(
       {this.adi, this.soyadi, this.eposta, this.kullaniciAdi, this.sifre});
 
-  RegisterModel.fromJson(Map<String, dynamic> json) {
-    adi = json['adi'];
-    soyadi = json['soyadi'];
-    eposta = json['eposta'];
-    kullaniciAdi = json['kullaniciAdi'];
-    sifre = json['sifre'];
+  factory RegisterModel.fromJson(Map<String, dynamic> json) {
+    return RegisterModel(
+      adi: json['adi'],
+      soyadi: json['soyadi'],
+      eposta: json['eposta'],
+      kullaniciAdi: json['kullaniciAdi'],
+      sifre: json['sifre'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['adi'] = adi;
-    data['soyadi'] = soyadi;
-    data['eposta'] = eposta;
-    data['kullaniciAdi'] = kullaniciAdi;
-    data['sifre'] = sifre;
-    return data;
+    return {
+      'adi': adi,
+      'soyadi': soyadi,
+      'eposta': eposta,
+      'kullaniciAdi': kullaniciAdi,
+      'sifre': sifre,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'RegisterModel(adi: $adi, soyadi: $soyadi, eposta: $eposta, kullaniciAdi: $kullaniciAdi, sifre: $sifre)';
   }
 }
